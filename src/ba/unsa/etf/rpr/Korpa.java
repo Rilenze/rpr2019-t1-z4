20dadc2857460d579cb8821cfd677d156092cf2c
Rijad <rburovic1@etf.unsa.ba>
2020-11-06 17:50:51 +0100

    2. commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int broj_artikala=0;
+    public Artikl[] getArtikli() { return artikli; }
+    public boolean dodajArtikl(Artikl a) {
+        if (broj_artikala == 50) return false;
+        artikli[broj_artikala] = a;
+        return true;
+    }
+    public int dajUkupnuCijenuArtikala() {
+        int suma=0;
+        for (int i=0; i<broj_artikala; i++) {
+            suma += artikli[i].getCijena();
+        }
+        return suma;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        boolean postoji = false;
+        int indeksIzbacenog = 0;
+        for (int i=0; i<broj_artikala; i++) {
+            if (artikli[i].getKod().equals(kod)) {
+                postoji = true;
+                indeksIzbacenog = i;
+                break;
+            }
+        }
+        if (!postoji) return null;
+        Artikl kopija = artikli[indeksIzbacenog];
+        for (int i = indeksIzbacenog; i < broj_artikala - 1; i++) {
+            artikli[i] = artikli[i+1];
+        }
+        broj_artikala--;
+        return kopija;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private static Artikl[] artikli = new Artikl[1000];
+    private static int broj_artikala=0;
+
+    public static boolean dodajArtikl(Artikl a) {
+        if (broj_artikala == 1000) return false;
+        artikli[broj_artikala] = a;
+        broj_artikala++;
+        return true;
+    }
+    public Artikl[] getArtikli() { return artikli; }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        boolean postoji = false;
+        int indeksIzbacenog = 0;
+        for (int i=0; i<broj_artikala; i++) {
+            if (artikli[i].getKod().equals(kod)) {
+                postoji = true;
+                indeksIzbacenog = i;
+                break;
+            }
+        }
+        if (!postoji) return null;
+        Artikl kopija = artikli[indeksIzbacenog];
+        for (int i = indeksIzbacenog; i < broj_artikala - 1; i++) {
+            artikli[i] = artikli[i+1];
+        }
+        broj_artikala--;
+        return kopija;
+    }
 }
